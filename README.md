2d5705c21c316152604b66503b5f671bc23a8aff
Miwtoo <miwyugioh@gmail.com>
2025-09-25 22:33:13 +0700

    feat: :sparkles: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# spend_mate
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/spend_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spend_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
